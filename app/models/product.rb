ec759d9a90c7f7c60996e6df38c193bf916fc14b
Adnan Furkan Aktemur <adnanfurkanaktemur@gmail.com>
2022-06-17 23:47:25 +0300

    user and product validates
    user and product validates added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
-
+  validates :name, presence: true, length: { minimum:2 }
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :compatibility, presence: true
+  validates :feature, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: { minimum: 2}
+  validates :surname, presence: true, length: { minimum: 2 }
+  validates :phone, presence: true, length: { minimum: 10}
+  validates :email, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: [:user, :moderator, :admin]

214447965eb7825747aaac57d39c02f2fed420e6
Adnan Furkan Aktemur <adnanfurkanaktemur@gmail.com>
2022-07-25 22:44:23 +0300

    require "rvm/capistrano"      require "bundler/capistrano"

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # Change these
 # config/deploy.rb
-
+require "rvm/capistrano"     
+require "bundler/capistrano"
 append :linked_dirs, '.bundle'
 server '164.92.160.202', port: 22, roles: [:web, :app, :db], primary: true
 
